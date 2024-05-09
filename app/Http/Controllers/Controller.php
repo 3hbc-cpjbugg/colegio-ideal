0aadededa954a5078b7c5502b9ea08cccfa85aeb
Cesar Perez <cpjbugg.8@gmail.com>
2024-05-08 20:52:02 -0500

    tu colegio ideal

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 /**
  * @OA\Info(
- *    title="Swagger with Laravel",
+ *    title="Cesar Perez :: Tu Colegio Ideal ",
  *    version="1.0.0",
  * )
  * @OA\SecurityScheme(
